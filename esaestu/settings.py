f839a30368042570c37bca41770a509f10716079
GSPLANET <gsplanet@gmail.com>
2025-12-25 08:34:15 +0000

    Initial commit from server

--- esaestu/settings.py
+++ esaestu/settings.py
@@ -11,21 +11,46 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # 2. Инициализируем environ
 env = environ.Env(
     # Здесь мы сразу задаем типы данных и значения по умолчанию
-    DEBUG=(bool, False) 
+    DEBUG=(bool, False)
 )
 
 # 3. Читаем файл .env, указывая путь к нему через BASE_DIR
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Вместо os.getenv('DEBUG') == 'True'
-DEBUG = env.bool('DEBUG', default=False)
+
+DEBUG = env('DEBUG')
 
 # 2. Безопасность и Отладка
 SECRET_KEY = env.str('SECRET_KEY')
 
 
 # Обработка хостов с защитой от пустого значения
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+#ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+# Пропиши это жестко, без env.list
+ALLOWED_HOSTS = ['esaestu.casa', 'www.esaestu.casa', '135.181.192.215']
+
+# Добавь это ПРЯМО ТУТ (не в конце файла, а рядом с ALLOWED_HOSTS)
+CSRF_TRUSTED_ORIGINS = ['https://esaestu.casa', 'https://www.esaestu.casa']
+
+
+# Включаем HSTS на 1 год (в секундах)
+SECURE_HSTS_SECONDS = 31536000 
+
+# Рекомендуется также добавить эти две настройки:
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Применять ко всем поддоменам
+SECURE_HSTS_PRELOAD = True             # Позволяет включить сайт в глобальный список HSTS
+
+
+# Запрещает встраивать твой сайт в <iframe> на чужих ресурсах (защита от кликджекинга)
+X_FRAME_OPTIONS = 'DENY'
+
+# Защита от подмены типов контента
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+
+
+
+
 
 
 # 3. Определение приложений
@@ -166,15 +191,17 @@ DEFAULT_FROM_EMAIL = env.str('EMAIL_HOST_USER', default='webmaster@localhost')
 if not DEBUG:
     # Перенаправлять все HTTP запросы на HTTPS
     SECURE_SSL_REDIRECT = True
-    
+
     # Защита куки сессии (передавать только по HTTPS)
     SESSION_COOKIE_SECURE = True
-    
+
     # Защита куки CSRF (передавать только по HTTPS)
     CSRF_COOKIE_SECURE = True
-    
+
     # Защита от подмены протокола за прокси-сервером (Nginx)
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    CSRF_TRUSTED_ORIGINS = ['https://esaestu.casa', 'https://www.esaestu.casa']
 
 # Настройка логирования только для сервера
 if not DEBUG:
@@ -204,5 +231,6 @@ if not DEBUG:
             },
         },
     }
+
 
 
